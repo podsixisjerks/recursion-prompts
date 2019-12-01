7567a8e5669500891f2b107b00ef2a86ba12ae90
Adam Gienapp <gienappa@gmail.com>
2019-12-01 10:23:29 -0800

    "solved thru exercise 12 (multiply)"

--- src/recursion.js
+++ src/recursion.js
@@ -199,6 +199,25 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+	if (y === 0 || x === 0){
+		return 0;
+	}
+	if (x > 0){
+		if (y > 0){
+			return x + multiply(x, y-1);
+		}
+		if (y < 0){
+			return -x + multiply(x, y+1);
+		}
+	}
+	if (x < 0){
+		if (y > 0){
+			return x + multiply(x, y-1);
+		}
+		if (y < 0){
+			return -x + multiply(x, y+1);
+		}
+	}
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
